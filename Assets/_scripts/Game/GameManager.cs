36e267abf5e111b7606942b55dfa6fe60444cf84
Nathan Blair <nathan.t.blair@outlook.com>
2016-01-28 23:48:37 -0600

    Cool new features: -Added the beginnings of a pause system -New game manager prefab will be used to handle game-wide events like pausing the game -Moved the player and associated prefabs to a shared Assets/Player/ directory

--- Assets/_scripts/Game/GameManager.cs
+++ Assets/_scripts/Game/GameManager.cs
@@ -3,13 +3,54 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	private bool _isPaused = false;
+	private bool _playerPause = false;
+	private CursorLockMode lockMode = CursorLockMode.Locked;
+
+	public GameObject player;
+	PlayerRotationInput playerRotationScript;
+	PlayerMovementInput playerMovementScript;
+
+	public bool GamePaused {
+		get {
+			return _isPaused;
+		}
+		set {
+			_isPaused = value;
+			Cursor.visible = value;
+			if (value) {
+				lockMode = CursorLockMode.None;
+			} else if (!value) {
+				lockMode = CursorLockMode.Confined;
+			}
+			Cursor.lockState = lockMode;
+		}
+	}
+
+	public bool PlayerPause {
+		get {
+			return _playerPause;
+		}
+		set {
+			_playerPause = value;
+			playerRotationScript.enabled = !value;
+			playerMovementScript.enabled = !value;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-		
+		playerRotationScript = player.GetComponent<PlayerRotationInput> ();
+		playerMovementScript = player.GetComponent<PlayerMovementInput> ();
+		GamePaused = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown ("Game Pause")) {
+			GamePaused = !GamePaused;
+			PlayerPause = !PlayerPause;
+		}
 	}
 }
